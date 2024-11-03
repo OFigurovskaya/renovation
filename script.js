16dcf9a767154c2ce4816bf7b1cc3a4624787f81
Olesya Figurovskaya <lisonkadrug@gmail.com>
2024-11-03 20:52:07 +0300

    header styled

---
+++ script.js
@@ -0,0 +1,45 @@
+//button close
+const close = document.querySelector('.calculation__close');
+
+close.addEventListener('click', () => {
+    const calculation = document.querySelector('.calculation');
+    calculation.style.display = 'none'
+})
+
+const sliderBig = document.querySelector('.repair__sliderBig');
+const slider = document.querySelector('.slider');
+let toggle = false;
+sliderBig.addEventListener('touchmove', slide, false);
+function slide()  {
+    slider.style.visibility =  toggle ? 'hidden' : 'visible';
+} 
+
+sliderBig.addEventListener('touchend', () => {
+    toggle = !toggle;
+})
+
+
+
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.wrapper',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
